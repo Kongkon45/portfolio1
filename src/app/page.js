d911dbdcd39a36d16f8329d01d3b5ea3856386be
Kongkon Jowarder <kongkon4545@gmail.com>
2025-01-03 11:56:11 +0600

    navbar, footer, about, hero, skill and contact section add

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
-import Image from 'next/image'
 import React from 'react'
-import myPic from '@/public/images/Kongkon.jpg'
+import HomePageAllComponents from '../components/home/home'
 
 export default function Home() {
   return (
     <div>
-      <h1 className='my-10'>This is Home page</h1>
-      <Image src={myPic} alt='my pic' width={300} height={300} placeholder='blur' blurDataURL='data...'/>
+      <HomePageAllComponents/>
     </div>
   )
 }
